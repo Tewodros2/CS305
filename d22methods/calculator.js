d32c86b472efeaf38c20083c34fd2c7b5137846b
Tewodros2 <84425319+Tewodros2@users.noreply.github.com>
2021-09-08 09:03:39 -0500

    Create calculator.js

---
+++ d22methods/calculator.js
@@ -0,0 +1,42 @@
+"use strict";
+
+
+/**
+ *  * @returns {Calculator} constructor function
+ */
+function Calculator() {
+    this.setValue = function Calculator(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+    /**
+     * 
+     * @returns{Number} sum;
+     */
+    this.sum = function () {
+        return this.a + this.b;
+    };
+
+    /**
+     * 
+     * @returns {Number} multiplication;
+     */
+    this.mul = function () {
+        return this.a * this.b;
+    }
+    /**
+     * 
+     * @returns {Number} subtraction;
+     */
+    this.sub = function () {
+        return this.a - this.b
+    }
+    /**
+     * 
+     * @returns {Number} division;
+     */
+    this.div = function () {
+        return this.a / this.b;
+    }
+};
+module.exports = { Calculator };
