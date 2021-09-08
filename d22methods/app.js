26b0e97cd2bbb1e9a936c4d19d0d4b1a43d512fb
Tewodros2 <84425319+Tewodros2@users.noreply.github.com>
2021-09-08 09:02:42 -0500

    Create app.js

---
+++ d22methods/app.js
@@ -0,0 +1,26 @@
+"use strict"
+/**
+ * 
+ * @param {*} currentValue 
+ * @param {*} increment 
+  * @returns {Accumulator} constructor function
+ */
+function Accumulator(currentValue, increment) {
+    this.currentValue = currentValue;
+    this.increment = increment;
+    /**
+     * 
+     * @returns current values+ increment
+     */
+    this.accumulate = function () {
+        return (this.currentValue += this.increment);
+    };
+    /**
+     * 
+     * @returns the current values
+     */
+    this.report = function () {
+        return this.currentValue;
+    };
+};
+module.exports = { Accumulator };
